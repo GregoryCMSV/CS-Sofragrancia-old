79a839cc33886add915e75d974903f8ced6e2bd6
Dev-Yudii <dev.yudii@gmail.com>
2026-05-15 12:27:46 -0300

    Update: Troca e adição do logo a topbar, 'Remoção' do menu lateral, reativação do icone para enviar para tela de alertas, e menu de configurações ao clicar no icone do gerente

--- Sofragrancia/Sofragrancia-Gerencial/Sofragrancia.UI/Components/Topbar.razor.cs
+++ Sofragrancia/Sofragrancia-Gerencial/Sofragrancia.UI/Components/Topbar.razor.cs
@@ -4,15 +4,28 @@ namespace Sofragrancia.UI.Components
 {
     public partial class Topbar
     {
+        // Injeta o serviço de navegação
+        [Inject] public NavigationManager Navigation { get; set; } = default!;
         [Parameter] public string Title { get; set; } = "Dashboard Geral";
         [Parameter] public string Breadcrumb { get; set; } = "Início > Dashboard";
 
         private DateTime DataInicio { get; set; } = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
         private DateTime DataFim { get; set; } = DateTime.Today;
 
+        // Controle do menu do usuário
+        private bool exibirMenu = false;
+        private void ToggleMenu() => exibirMenu = !exibirMenu;
+
         private void Filtrar()
         {
             Console.WriteLine($"Filtrando de {DataInicio.ToShortDateString()} até {DataFim.ToShortDateString()}");
+        }
+
+        // Método para navegar e fechar o menu
+        private void IrPara(string rota)
+        {
+            exibirMenu = false;
+            Navigation.NavigateTo(rota);
         }
     }
 }
