ad75028488a47a7fec790667b1dc9273b642aebd
GregoryCMSV <121581844+GregoryCMSV@users.noreply.github.com>
2026-05-18 21:48:28 -0300

    Ajustado repository de cada alerta
    Ajustado repository de cada alerta

--- Sofragrancia/Sofragrancia-Gerencial/Sofragrancia.API/Controllers/AlertaController.cs
+++ Sofragrancia/Sofragrancia-Gerencial/Sofragrancia.API/Controllers/AlertaController.cs
@@ -4,7 +4,7 @@ using Supabase;
 
 namespace Sofragrancia.API.Controllers
 {
-    public class AlertaController : BaseController<AlertaBase, AlertaRepository>
+    public class AlertaController : BaseController<AlertaBase, AlertaHeaderRepository>
     {
         public AlertaController(Client client) : base(client)
         {
---
+++ Sofragrancia/Sofragrancia-Gerencial/Sofragrancia.Banco/Repositories/AlertaHeaderRepository.cs
@@ -0,0 +1,27 @@
+﻿using Sofragrancia.Banco.Models.Alertas;
+using Supabase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sofragrancia.Banco.Repositories
+{
+    public class AlertaBaseRepository : Repository<AlertaBase>
+    {
+        public AlertaBaseRepository(Client supabase) : base(supabase)
+        {
+        }
+    }
+    public class AlertaConfigUserRepository : Repository<AlertaConfigUser>
+    {
+        public AlertaConfigUserRepository(Client supabase) : base(supabase)
+        {
+        }
+    }
+    public class AlertaHeaderRepository : Repository<AlertaHeader>
+    {
+        public AlertaHeaderRepository(Client supabase) : base(supabase)
+        {
+        }
+    }
+}
