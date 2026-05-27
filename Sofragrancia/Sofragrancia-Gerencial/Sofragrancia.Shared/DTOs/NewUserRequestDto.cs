fb190bd521416cffbffac89f18cd42edecb6c84d
GregoryCMSV <121581844+GregoryCMSV@users.noreply.github.com>
2026-05-27 03:12:25 -0300

    Criado DTO para cadastro de novos usuários
    Criado DTO para cadastro de novos usuários

---
+++ Sofragrancia/Sofragrancia-Gerencial/Sofragrancia.Shared/DTOs/NewUserRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sofragrancia.Shared.DTOs
+{
+    public record NewUserRequestDto
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Role { get; set; } 
+        public string NomeCompleto { get; set; }
+    }
+}
