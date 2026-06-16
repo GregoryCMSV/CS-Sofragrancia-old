d4b93b6101459940f363f40050d385393332fb12
GregoryCMSV <47572621+PehGabrielVilar@users.noreply.github.com>
2026-06-16 19:26:21 -0300

    update index
    update index

--- Sofragrancia/Sofragrancia-Gerencial/Sofragrancia.UI/Pages/Index.razor.cs
+++ Sofragrancia/Sofragrancia-Gerencial/Sofragrancia.UI/Pages/Index.razor.cs
@@ -6,7 +6,7 @@ namespace Sofragrancia.UI.Pages
     public partial class Index
     {
 
-        private string source = "https://app.powerbi.com/view?r=eyJrIjoiYzllNmU4NjQtOWU0YS00MGQ1LTgwZDQtMDYxMGVjMzA3NTFjIiwidCI6ImVhYmU2NGM1LTY4ZjUtNGE3Ni04MzAxLTk1NzdhNjc5ZTQ0OSIsImMiOjR9";
+        private string source = "https://app.powerbi.com/view?r=eyJrIjoiMWQ1ZGUxYTItMDc4Zi00MzM4LTgyNzAtYjE3ODdmNzlhMTljIiwidCI6ImNmNzJlMmJkLTdhMmItNDc4My1iZGViLTM5ZDU3YjA3Zjc2ZiIsImMiOjR9";
 
         protected override void OnInitialized()
         {
